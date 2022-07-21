4c2a2882d7c52c0d6f808fff95677a15429cba45
Nathan Hood <nathanjhood@googlemail.com>
2022-07-21 04:50:09 +0200

    src file

---
+++ ConsoleApplication/src/ConsoleApplication.h
@@ -0,0 +1,51 @@
+// ConsoleApplication.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <typeinfo>
+
+// TODO: Reference additional headers your program requires here.
+
+//#include "Atomic.h"
+#include "Float.h"
+#include "UniquePointer.h"
+#include "Value.h"
+//#include "X.h"
+
+void intro()
+{
+    std::cout << "Hello CMake World!" << std::endl;
+    std::cout << std::endl;
+}
+
+void outro()
+{
+    std::cout << "Goodbye CMake World!" << std::endl;
+    std::cout << std::endl;
+}
+
+void wait()
+{
+    std::cout << "Press ''Enter'' to continue!" << std::endl;
+    std::cin.get();
+}
+
+void newLine()
+{
+    std::cout << std::endl;
+}
+
+void lineBreak()
+{
+    std::cout << "//============================================================================//" << std::endl;
+}
+
+void endFunction()
+{
+    lineBreak();
+    newLine();
+    wait();
+    newLine();
+}
