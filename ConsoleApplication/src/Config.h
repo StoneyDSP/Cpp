5340918259a2b26c91f29b9fd43c1e44d5d75abc
Nathan Hood <nathanjhood@googlemail.com>
2022-08-15 05:01:41 +0200

    Added CMake config'd header file

---
+++ ConsoleApplication/src/Config.h
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * 
+ * @file Config.h.in
+ * 
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @version 1.0.0.1
+ * @date 2022-08-13
+ * 
+ * @copyright Copyright (c) 2022 @StoneyDSP
+ * 
+ *******************************************************************************
+*/
+
+#pragma once
+
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+#define projectName "ConsoleApplication"
+#define projectVersion "v1.0.0.1"
+#define website "www.github.com/StoneyDSP"
+#define V_Major 1
+#define V_Minor 0
+#define V_Patch 0
+#define V_Tweak 1
+#define sourceDir "C:/Cpp/ConsoleApplication"
+#define currentSourceDir "C:/Cpp/ConsoleApplication"
+#define binaryDir "C:/Cpp/ConsoleApplication/Builds/CMake/x64-debug"
+#define currentBinaryDir "C:/Cpp/ConsoleApplication/Builds/CMake/x64-debug"
+
+#endif // CONFIG_H_INCLUDED
