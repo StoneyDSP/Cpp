2870090f930f7ce24cc4acea0b64d26b75e09311
Nathan Hood <nathanjhood@googlemail.com>
2022-08-17 06:56:16 +0200

    Improved main .h/.cpp

--- CMakeProject1/Source/Main.cpp
+++ CMakeProject1/Source/Main.cpp
@@ -1,9 +1,24 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-08-17
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
 #include "main.h"
 
+/**
+ * @brief Entry point for the application.
+ *
+ * @return int
+ */
 int main()
 {
-    std::cout << "Built by CMake!" << std::endl;
+    std::cout << "Built for " << companyName <<" by CMake!" << std::endl;
 
     return 0;
 }
--- CMakeProject1/Source/Main.h
+++ CMakeProject1/Source/Main.h
@@ -1,2 +1,14 @@
+/**
+ * @file main.h
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-08-17
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
 #include <iostream>
+
+#include <cfg/Config.h>
