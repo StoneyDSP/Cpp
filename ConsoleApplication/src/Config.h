537dff45c24183e14edfc7497f31488467c870fa
Nathan Hood <nathanjhood@googlemail.com>
2022-08-17 02:39:36 +0200

    Added #define projectAuthor to config

--- ConsoleApplication/src/Config.h
+++ ConsoleApplication/src/Config.h
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * 
+ *
  * @file Config.h.in
- * 
+ *
  * @author Nathan J. Hood (nathanjhood@googlemail.com)
  * @version 1.0.0.1
  * @date 2022-08-13
- * 
+ *
  * @copyright Copyright (c) 2022 @StoneyDSP
- * 
+ *
  *******************************************************************************
 */
 
@@ -16,6 +16,7 @@
 #ifndef CONFIG_H_INCLUDED
 #define CONFIG_H_INCLUDED
 
+#define projectAuthor "Nathan J. Hood"
 #define projectName "ConsoleApplication"
 #define projectVersion "v1.0.0.1"
 #define website "www.github.com/StoneyDSP"
@@ -25,7 +26,7 @@
 #define V_Tweak 1
 #define sourceDir "C:/Cpp/ConsoleApplication"
 #define currentSourceDir "C:/Cpp/ConsoleApplication"
-#define binaryDir "C:/Cpp/ConsoleApplication/Builds/CMake/x64-debug"
-#define currentBinaryDir "C:/Cpp/ConsoleApplication/Builds/CMake/x64-debug"
+#define binaryDir "C:/Cpp/ConsoleApplication"
+#define currentBinaryDir "C:/Cpp/ConsoleApplication"
 
 #endif // CONFIG_H_INCLUDED
