7c0cf4c6ef56284ae96e8bb79fc873f1de0700fd
Nathan Hood <nathanjhood@googlemail.com>
2022-08-19 05:35:26 +0200

    Small update

---
+++ ConsoleApplication/cfg/Config.h
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ *
+ * @file Config.h.in
+ *
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @version 1.0.0.1
+ * @date 2022-08-13
+ *
+ * @copyright Copyright (c) 2022 @StoneyDSP
+ *
+ *******************************************************************************
+*/
+
+#pragma once
+
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+#define companyName "StoneyDSP"
+#define projectName "ConsoleApplication"
+#define projectVersion "v1.0.0.2"
+#define website "www.github.com/StoneyDSP"
+#define V_Major 1
+#define V_Minor 0
+#define V_Patch 0
+#define V_Tweak 2
+#define sourceDir "C:/Cpp/ConsoleApplication"
+#define currentSourceDir "C:/Cpp/ConsoleApplication"
+#define binaryDir "/bin/"
+#define currentBinaryDir "C:/Cpp/ConsoleApplication/bin/x64"
+#define homeDir "C:/Cpp/ConsoleApplication"
+#define makeCommand ""
+
+#endif // CONFIG_H_INCLUDED
